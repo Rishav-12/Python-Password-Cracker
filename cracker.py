dc7ef8909a49610500cbdacd334f79a9a4f8d2a7
Rishav Mitra <rishavmitra10@gmail.com>
2021-08-24 12:18:26 +0000

    using generator to read file
    Used a custom generator to avoid using a data structure to store all passwords from the file

--- cracker.py
+++ cracker.py
@@ -1,5 +1,9 @@
 import hashlib
 
+def file_reader(filename):
+	for row in open(filename, 'r'):
+		yield row
+
 def text_to_sha256(text):
 	sha256_hashed_text = hashlib.sha256(text.encode()).hexdigest()
 	return sha256_hashed_text
@@ -8,17 +12,19 @@ def main():
 	target_sha256 = input("Enter the SHA256 hash to crack: ").strip().lower()
 	passwords_file = input("Enter the wordlist to look in: ")
 
-	with open(passwords_file, 'r') as f:
-		passwords = f.readlines()
+	reader = file_reader(passwords_file)
 
-	for password in passwords:
-		cleaned_password = password.strip()
-		password_sha256 = text_to_sha256(cleaned_password)
+	while True:
+		try:
+			cleaned_password = next(reader).strip()
+			password_sha256 = text_to_sha256(cleaned_password)
 
-		if password_sha256 == target_sha256:
-			print("Password found")
-			print(f"Password: {cleaned_password}")
-			return
+			if password_sha256 == target_sha256:
+				print("Password found")
+				print(f"Password: {cleaned_password}")
+				return
+		except StopIteration:
+			break
 	print("Password not found in given file")
 
 if __name__ == '__main__':
